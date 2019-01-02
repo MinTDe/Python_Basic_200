d2457536c46e813212e2549349a67b8bc8b3752a
mintde <yc424k@gmail.com>
2019-01-03 00:09:43 +0900

    Indentation

--- 008.Indentation/main.py
+++ 008.Indentation/main.py
@@ -1,5 +1,8 @@
 list_data = [1,2,3]
 
+#파이썬에는 코드를 들여쓰기(Indentation)으로 구분한다
+#들여쓰기 규칙1. 첫 줄 코드는 들여쓰기 없이 시작한다
+#들여쓰기 규칙2. : 이후 시작하는 실행 코드 들여쓰기는 간격이 일정해야된다
 if 1 in list_data:
     print(list_data)
     print('1 in list_data')
