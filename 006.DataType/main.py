cb335138c55cabdc0f36750297f632831a0c554f
mintde <yc424k@gmail.com>
2019-01-02 23:58:35 +0900

    DataTypl

---
+++ 006.DataType/main.py
@@ -0,0 +1,18 @@
+Int_data = 1                        #정수 선언
+Float_data = 1.2                    #실수 선언
+Complex_data = 1 + 4j;              #복소수 선언
+String_data1 = 'Hello World!'       #문자열 선언
+String_data2 = "Hello World!"       #문자열 선언
+String_data3= """Hello World!"""    #문자열 선언
+list_data = [1,2,3]                 #리스트 선언
+tuple_data = (1,2,3)                #튜블 선언
+dict_data = {0:'Flase',1:'True'}    #사전 선언
+print(Int_data)
+print(Float_data)
+print(Complex_data)
+print(String_data1)
+print(String_data2)
+print(String_data3)
+print(list_data[0])
+print(tuple_data)
+print(dict_data[0])
