77fb20bc97d06cc3c2a8a13a5e3b329f935faafe
mintde <yc424k@gmail.com>
2019-01-03 15:48:44 +0900

    Complex number

--- 019.Complexnumber/main.py
+++ 019.Complexnumber/main.py
@@ -3,5 +3,5 @@ complex_number1 = 1 + 7j
 print(complex_number1.real) #복소수 실수부만 출력
 print(complex_number1.imag) #복소수 허수부만 출력
 
-complex_number2 = complex(2,3) ##실수부가 2 허수부가 3인 복소수를 생성
+complex_number2 = complex(2,3) #실수부가 2 허수부가 3인 복소수를 생성
 print(complex_number2)
