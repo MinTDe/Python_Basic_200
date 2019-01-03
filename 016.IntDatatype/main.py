df0ecdf13dae9a8fb37ed771f8a2a7505e19bbc8
mintde <yc424k@gmail.com>
2019-01-03 15:23:47 +0900

    Int datatype

---
+++ 016.IntDatatype/main.py
@@ -0,0 +1,15 @@
+#정수형 datatype들
+int_data = 10
+print(int_data)
+#2진법
+bin_data = 0b10
+print(bin_data)
+#8진법
+oct_data = 0o10
+print(oct_data)
+#16진법
+hex_data = 0x10
+print(hex_data)
+#python은 메모리가 허용하는 범위에서 지원 가능한 수를 사용할 수 있다.
+long_data = 1234567890987654321
+print(long_data)
