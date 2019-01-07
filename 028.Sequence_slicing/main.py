3a96568cb103eeccbf7764959f02fb8ec6042d69
mintde <yc424k@gmail.com>
2019-01-07 14:41:22 +0900

    Slicing

---
+++ 028.Sequence_slicing/main.py
@@ -0,0 +1,9 @@
+#시퀀스 슬라이싱 예시
+str1 = 'Hello World!'
+#변수[x:y]
+print(str1[0:])     #인덱스 x부터 출력
+print(str1[:3])     #인덱스 y까지 출력
+print(str1[1:3])    #인덱스 x부터 y까지 출력
+print(str1[-4:])    #끝에서 x까지 출력
+print(str1[:-5])    #처음부터 y까지 출력
+print(str1[:])      #전체 출력
