d0904906c90c009b9266acd19745d0c903534998
mintde <yc424k@gmail.com>
2019-01-07 14:27:04 +0900

    Sequence data type

---
+++ 026.Sequence_data_type/main.py
@@ -0,0 +1,9 @@
+#시퀀스 자료형 예시들
+str_data = 'abc'
+list_data = [1,2,[3,4],5]
+tuple_data = (100,200,300)
+
+print(str_data)
+print(list_data)
+print(tuple_data)
+#시퀀스 자료형 특성에는 대표적으로 인덱싱, 슬라이싱, 연결, 반복, 멤버체크, 크기정보가 있다
