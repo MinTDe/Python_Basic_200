1f591e6237b4231e7d2ca7d5d49870ac0268a9aa
mintde <yc424k@gmail.com>
2019-01-07 17:05:26 +0900

    function

---
+++ 039.Function(def)/main.py
@@ -0,0 +1,23 @@
+#함수 사용 예시
+def add(n1, n2):
+    return n1 + n2
+
+print(add(1,2))
+
+#인자 O 리턴값 O
+#def name(arg1, arg2...)
+#코드
+#return 값
+
+#인자 X 리턴값 O
+#def name()
+#코드
+#return 값
+
+#인자 O 리턴값 X
+#def name(arg1, arg2...)
+#코드
+
+#인자 X 리턴값 X
+#def name()
+#코드
