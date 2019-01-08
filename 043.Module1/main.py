c21b1cd50e0b69e99f0f8b5f83ad2b2742574bbd
mintde <yc424k@gmail.com>
2019-01-08 14:48:50 +0900

    Make Module

---
+++ 043.Module1/main.py
@@ -0,0 +1,9 @@
+#모듈 사용 예시
+#모듈 생성 예시
+import mylib
+
+ret1 = mylib.add(1,3)
+print(ret1)
+
+ret2 = mylib.reverse(1, 6, 9)
+print(ret2)
---
+++ 043.Module1/mylib.py
@@ -0,0 +1,6 @@
+#모듈 생성 페이지
+def add(t1, t2) :
+    return t1 + t2
+
+def reverse(x, y, z) :
+    return z, y, x
