a809bb21daa304a0592c58d2d2d1f760460bec63
mintde <yc424k@gmail.com>
2019-01-08 15:35:12 +0900

    Structure

---
+++ 045.Module3/main.py
@@ -0,0 +1,18 @@
+#모듈 사용 예시
+#모듈 생성 예시
+#패키지 사용 예시
+#패키지 생성 예시
+
+#import package_name.module_name
+import mypackage.mylib
+
+#import module_name
+import sleep.time
+
+time.sleep(1)
+
+ret1 = mypackage.mylib.add(1,3)
+print(ret1)
+
+ret2 = mypackage.mylib.reverse(1, 6, 9)
+print(ret2)
