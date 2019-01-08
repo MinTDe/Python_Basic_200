fd6a26a2e065fb456227ec1d648debb947f93b6b
mintde <yc424k@gmail.com>
2019-01-08 16:35:13 +0900

    from ~ import

--- 046.Module4/main.py
+++ 046.Module4/main.py
@@ -2,23 +2,22 @@
 #모듈 생성 예시
 #패키지 사용 예시
 #패키지 생성 예시
+#from ~ import
 
-#import package_name.module_name
-#import mypackage.mylib
 from mypackage import mylib
-from mypackage.mylib import reverse
 
-#import module_name
-#import sleep.time
+from mypackage.mylib import reverse
+#from package_name import module_name
+
+
 from time import  sleep
+#from module_name import function_name
 
 #time.sleep(1)
 sleep(1)
 
-#ret1 = mypackage.mylib.add(1,3)
 ret1 = mylib.add(1, 3)
 print(ret1)
 
-#ret2 = mypackage.mylib.reverse(1, 6, 9)
 ret2 = reverse(1,3,5)
 print(ret2)
