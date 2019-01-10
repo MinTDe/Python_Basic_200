dd05cb5e66653d0c9658df735046dc96c62ca4ce
mintde <yc424k@gmail.com>
2019-01-10 16:14:41 +0900

    Class Basic

--- 049.Class1/main.py
+++ 049.Class1/main.py
@@ -1,6 +1,7 @@
 #Class 선언 예시
-class Class :               #class class_name :
-    var = 'Hello'           #class 멤버 정의
+class Class :
+    def __init__(self) :              #class class_name :
+        self.var = 'Hello'           #class 멤버 정의
     def Hello(self) :       #class 메소드 정의
         print(self.var)
 
