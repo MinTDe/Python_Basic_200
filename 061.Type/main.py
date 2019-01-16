648d7f066914ae46b6d42e8e2d19b10eab736e4a
mintde <yc424k@gmail.com>
2019-01-16 15:53:34 +0900

    type function

---
+++ 061.Type/main.py
@@ -0,0 +1,13 @@
+num_data = 123
+str_data = 'python'
+list_data = [1,2,3]
+dic_data = {'a':1, 'b':2}
+
+def func():
+    print('Hello World!')
+
+print(type(num_data))
+print(type(str_data))
+print(type(list_data))
+print(type(dic_data))
+print(type(func))
