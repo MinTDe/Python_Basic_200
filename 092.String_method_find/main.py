42eff0aa2daf2ae0663b331df3ff8661a08a9ca2
mintde <yc424k@gmail.com>
2019-01-21 21:21:17 +0900

    method 'find'

--- 092.String_method_find/main.py
+++ 092.String_method_find/main.py
@@ -5,8 +5,10 @@ txt = 'The Internet of Things'
 
 result1 = txt.find('of')
 result2 = txt.find('Th')
-result3 = txt.find('Th', 10)
+result3 = txt.find('Th', 10)    #10번째 이후부터 'Th'를 찾음
+result4 = txt.find('Hello')
 
 print(result1)
 print(result2)
 print(result3)
+print(result4)                  #해당 문자나 문자열을 찾지 못하면 -1를 return함
