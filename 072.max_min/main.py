29d7db469615ac0b2c170e5376a2358828587756
mintde <yc424k@gmail.com>
2019-01-21 17:02:37 +0900

    max min function

---
+++ 072.max_min/main.py
@@ -0,0 +1,11 @@
+#max min 함수 예시
+#max 최대값을 구하는 함수
+#min 최솟값을 구하는 함수
+
+list_data = [1,2,3,4,5,6,7,8,9,0]
+print(max(list_data))
+print(min(list_data))
+
+txt = 'Hello World!'
+print(max(txt))
+print(min(txt))
