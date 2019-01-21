f8923135f5f39658faf67685dd17700ecb362581
mintde <yc424k@gmail.com>
2019-01-21 20:10:11 +0900

    function 'isalpha'

---
+++ 084.String_function_isalpha/main.py
@@ -0,0 +1,17 @@
+#뮨자열이 알파벳인지 아닌지 확인하는 함수
+#함수 'isalpha'
+
+txt1 = 'Hello'
+txt2 = '1234'
+txt3 = '안녕하세요' #언어로만 구성되어 있기 때문에 True가 출력된다
+txt4 = 'starcraft2'
+
+result1 = txt1.isalpha()
+result2 = txt2.isalpha()
+result3 = txt3.isalpha()
+result4 = txt4.isalpha()
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
