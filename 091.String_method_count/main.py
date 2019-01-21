d85f1ee6750ea52236d14fdc4dfaa4a2d5dfe229
mintde <yc424k@gmail.com>
2019-01-21 21:15:31 +0900

    method count

---
+++ 091.String_method_count/main.py
@@ -0,0 +1,12 @@
+#문자열에 있는 문자 갯수 구하는 함수
+#함수 = 'count'
+
+txt = 'My name is MinTde'
+
+word_count1 = txt.count('s')
+word_count2 = txt.count('name')
+word_count3 = txt.count(' ')
+
+print(word_count1)
+print(word_count2)
+print(word_count3)
