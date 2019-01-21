ed9c7576b9d9ab2d663bec4a4a8bf89d69b67ce0
mintde <yc424k@gmail.com>
2019-01-21 20:26:58 +0900

    function 'isalnum'

---
+++ 086.String_function_isalnum/main.py
@@ -0,0 +1,14 @@
+#문자열이 문자 또는 숫자로만 이루져있는 검사하는 함수
+#함수 = 'isalnum'
+
+txt1 = "Hello?"
+txt2 = "Hello World!"
+txt3 = "id123"
+
+result1 = txt1.isalnum() #'?'가 있기 때문에 결과 값은 false
+result2 = txt2.isalnum() #' '와 '!가 있기 때문에 결과 값은 false'
+result3 = txt3.isalnum()
+
+print(result1)
+print(result2)
+print(result3)
