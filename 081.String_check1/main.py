7b33d0f8152ae75c08454c55b221954fa12b3d5b
mintde <yc424k@gmail.com>
2019-01-21 20:02:47 +0900

    String check using 'in'

---
+++ 081.String_check1/main.py
@@ -0,0 +1,8 @@
+#문자열에 문자가 있는지 확인하는 예시
+str1 = input(('Write String : '))
+
+if 'e' in str1:
+    print('e is in your string')
+
+else:
+    print('e is not in your string')
---
+++ 082.String_check2/main.py
@@ -0,0 +1,8 @@
+#문자열에 문자열이 있는지 확인하는 예시
+str1 = input(('Write String : '))
+
+if 'World!' or 'world!' in str1:
+    print('\'World!\' is in your string')
+
+else:
+    print('\'World!\' is not in your string')
