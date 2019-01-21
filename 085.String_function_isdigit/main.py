7e76b5e253d041ed6785333fb7ac41f02147747c
mintde <yc424k@gmail.com>
2019-01-21 20:12:53 +0900

    function 'isdigit'

---
+++ 085.String_function_isdigit/main.py
@@ -0,0 +1,14 @@
+#문자열이 숫자인지 검사하는 함수
+#함수 = 'isdigit'
+
+txt1 = '112'
+txt2 = '112-119'        #'-' 기호가 들어가있기 때문에 false가 출력된다.
+txt3 = 'Hello World!'   #문자로 이루어져 있는 문자열이기 때문에 false가 출력된다.
+
+result1 = txt1.isdigit()
+result2 = txt2.isdigit()
+result3 = txt3.isdigit()
+
+print(result1)
+print(result2)
+print(result3)
