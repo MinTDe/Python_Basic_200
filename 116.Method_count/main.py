4f18b18d405e312bb980e5cbd04c518113eb1683
mintde <yc424k@gmail.com>
2019-01-23 17:59:39 +0900

    Method 'count'

---
+++ 116.Method_count/main.py
@@ -0,0 +1,12 @@
+#리스트에 특정 요소의 개수를 찾을 때 사용하는 메소드
+#메소드 'count'
+
+list_data1 = ['a', 'b', 'c', 'a', 'b', 'c', 'a', 'b', 'c', 'a', 'a', 'a']
+
+listcount1 = list_data1.count('a')
+listcount2 = list_data1.count('b')
+listcount3 = list_data1.count('c')
+
+print('a : %d' %listcount1)
+print('b : %d' %listcount2)
+print('c : %d' %listcount3)
