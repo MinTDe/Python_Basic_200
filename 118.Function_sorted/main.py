b3b9bdb09beb4956fda8d6e2e06e82a8b6ac6c6f
mintde <yc424k@gmail.com>
2019-01-23 18:25:25 +0900

    function 'sorted'

---
+++ 118.Function_sorted/main.py
@@ -0,0 +1,10 @@
+#정렬 함수
+#함수 'sorted'
+
+alpahbet_list = ['q', 'a', 'z', 'w', 'e', 'r', 's', 'd', 'f', 'x', 'c', 'v', 't', 'g', 'b', 'y', 'h', 'n', 'u', 'i', 'o', 'j', 'k', 'l', 'm', 'p']
+
+list = sorted(alpahbet_list)
+
+
+print(alpahbet_list)        #원래 데이터를 유지
+print(list)
