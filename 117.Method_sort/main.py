298b51a6a9ebe7231a9f236abf6444d64befdc21
mintde <yc424k@gmail.com>
2019-01-23 18:23:45 +0900

    method 'sort'

---
+++ 117.Method_sort/main.py
@@ -0,0 +1,9 @@
+#정렬 메소드
+#메소드 'sort'
+
+alpahbet_list = ['q', 'a', 'z', 'w', 'e', 'r', 's', 'd', 'f', 'x', 'c', 'v', 't', 'g', 'b', 'y', 'h', 'n', 'u', 'i', 'o', 'j', 'k', 'l', 'm', 'p']
+
+alpahbet_list.sort()
+
+
+print(alpahbet_list)        #원래 데이터를 변형
